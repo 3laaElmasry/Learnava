055380efb5a673132c5634d93aedfbdebca26a2f
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-24 20:47:51 +0300

    Creat Entites DbSet & Apply Fluent Api Configruations

--- Learnava.DataAccess/Data/ApplicationDbConetxt.cs
+++ Learnava.DataAccess/Data/ApplicationDbConetxt.cs
@@ -1,6 +1,10 @@
 ﻿
+using Learnava.DataAccess.Data.Configurations;
+using Learnava.DataAccess.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
 
 namespace Learnava.DataAccess.Data
 {
@@ -11,6 +15,16 @@ namespace Learnava.DataAccess.Data
             
         }
 
+        public DbSet<Instructor> instructors { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Enrollment> enrollments { get; set; }  
 
+        public DbSet<Student> students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(StudentConfigurations).Assembly);
+
+        }
     }
 }
