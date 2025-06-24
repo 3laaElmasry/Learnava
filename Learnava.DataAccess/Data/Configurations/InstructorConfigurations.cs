d335624b79b3bac875422e56d205828d100bcfed
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-24 19:30:30 +0300

    Instructor Configurations

---
+++ Learnava.DataAccess/Data/Configurations/InstructorConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using Learnava.DataAccess.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Learnava.DataAccess.Data.Configurations
+{
+    public class InstructorConfigurations : IEntityTypeConfiguration<Instructor>
+    {
+        public void Configure(EntityTypeBuilder<Instructor> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.HasOne(i => i.ApplicationUser)
+                .WithOne()
+                .HasForeignKey<Instructor>(i => i.ApplicationUserId)
+                .IsRequired();
+
+        }
+    }
+}
