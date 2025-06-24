ecbf4f714ab04865347401c250ecde9021148598
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-24 19:33:38 +0300

    Student Configurations

---
+++ Learnava.DataAccess/Data/Configurations/StudentConfigurations.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Learnava.DataAccess.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Learnava.DataAccess.Data.Configurations
+{
+    public class StudentConfigurations : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasOne(x => x.ApplicationUser)
+                .WithOne()
+                .HasForeignKey<Student>(s => s.ApplicationUserId)
+                .IsRequired();
+
+
+        }
+    }
+}
