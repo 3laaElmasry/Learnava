78edc1385e5b01bc671809a8725d966a22fd3a32
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-24 20:42:15 +0300

    Course Configurations

---
+++ Learnava.DataAccess/Data/Configurations/CourseConfigurations.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Learnava.DataAccess.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Learnava.DataAccess.Data.Configurations
+{
+    public class CourseConfigurations : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Title)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(c => c.Description)
+               .HasMaxLength(200)
+               .IsRequired();
+
+            builder.Property(c => c.CreatedAt)
+               .IsRequired();
+
+
+            builder.HasOne(c => c.Instructor)
+                .WithMany()
+                .HasForeignKey(c => c.InstructorId)
+                .IsRequired();
+        }
+    }
+}
