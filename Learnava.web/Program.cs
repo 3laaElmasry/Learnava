25c6277a1bd5dd9b1af99e27834ca721b1d13e82
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-24 21:00:16 +0300

    Conncetion String Configuration

--- Learnava.web/Program.cs
+++ Learnava.web/Program.cs
@@ -1,9 +1,15 @@
 
+using Learnava.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbConetxt>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
