3496efd031d19548d75b7e47f7fa48a9a28e77d8
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-28 16:55:53 +0300

    Create Student Service

---
+++ Learnava.BusinessLogic/IServiceContracts/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Learnava.DataAccess.Data.Entities;
+
+namespace Learnava.BusinessLogic.IServiceContracts
+{
+    public interface IStudentService
+    {
+        Task<Student> Create(string userId);
+    }
+}
---
+++ Learnava.BusinessLogic/Services/StudentService.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Learnava.BusinessLogic.IServiceContracts;
+using Learnava.DataAccess.Data.Entities;
+using Learnava.DataAccess.RepositoryContracts;
+
+namespace Learnava.BusinessLogic.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentService(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<Student> Create(string userId)
+        {
+            var newStudent = new Student()
+            {
+                ApplicationUserId = userId,
+            };
+            await _studentRepository.AddAsync(newStudent);
+            await _studentRepository.Save();
+            return newStudent;
+        }
+    }
+}
