336a6cf27a550ce5ef4a19c098f900cfe73d49ca
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-28 16:19:13 +0300

    Enrollment Repository

---
+++ Learnava.DataAccess/Repository/EnrollmentRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Learnava.DataAccess.Data;
+using Learnava.DataAccess.Data.Entities;
+using Learnava.DataAccess.RepositoryContracts;
+
+namespace Learnava.DataAccess.Repository
+{
+    public class EnrollmentRepository : Repository<Enrollment>, IEnrollmentRepository
+    {
+        public EnrollmentRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
