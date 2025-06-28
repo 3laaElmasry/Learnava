323a8ac2e8e48f99892d33cf6656044b7f82e276
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-28 16:17:28 +0300

    Course Repository

---
+++ Learnava.DataAccess/Repository/CourseRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Learnava.DataAccess.Data;
+using Learnava.DataAccess.Data.Entities;
+using Learnava.DataAccess.RepositoryContracts;
+
+namespace Learnava.DataAccess.Repository
+{
+    public class CourseRepository : Repository<Course>, ICourseRepository
+    {
+        public CourseRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
---
+++ Learnava.DataAccess/RepositoryContracts/ICourseRepository.cs
@@ -0,0 +1,10 @@
+﻿
+
+using Learnava.DataAccess.Data.Entities;
+
+namespace Learnava.DataAccess.RepositoryContracts
+{
+    public interface ICourseRepository : IRepository<Course>
+    {
+    }
+}
