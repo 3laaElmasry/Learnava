8ded1f0605f39e0e1fe40f80e1b5922560bb8c58
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-28 16:26:09 +0300

    Implment Create Instuctor Service

--- Learnava.BusinessLogic/Services/InstructorService.cs
+++ Learnava.BusinessLogic/Services/InstructorService.cs
@@ -2,14 +2,31 @@
 
 using Learnava.BusinessLogic.IServiceContracts;
 using Learnava.DataAccess.Data.Entities;
+using Learnava.DataAccess.RepositoryContracts;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace Learnava.BusinessLogic.Services
 {
     public class InstructorService : IInstructorService
     {
+        private readonly IInstructorRepository _instructorRepository;
+
+        public InstructorService(IInstructorRepository instructorRepository)
+        {
+            _instructorRepository = instructorRepository;
+        }
+
         public async Task<Instructor> Create(string userID)
         {
-            return  new Instructor();
+            var newInstructor = new Instructor()
+            {
+                ApplicationUserId = userID,
+
+            };
+
+            await _instructorRepository.AddAsync(newInstructor);
+            await _instructorRepository.Save();
+            return newInstructor;
         }
     }
 }
