c4b1a30dd92cdf9c50f445cf942c8ba3c913b88b
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-28 16:15:40 +0300

    Student Repository

---
+++ Learnava.DataAccess/Repository/StudentRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Learnava.DataAccess.Data;
+using Learnava.DataAccess.Data.Entities;
+using Learnava.DataAccess.RepositoryContracts;
+
+namespace Learnava.DataAccess.Repository
+{
+
+    public class StudentRepository : Repository<Student>, IStudentRepository
+    {
+        public StudentRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
