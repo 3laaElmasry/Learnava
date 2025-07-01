4b0e87687a3aac48f9eb112f43754c070c276f81
Elmasry <3laaelmasry2005a@gmail.com>
2025-07-02 00:48:14 +0300

    Delete Seeding of Courses

--- Learnava.DataAccess/Data/Configurations/CourseConfigurations.cs
+++ Learnava.DataAccess/Data/Configurations/CourseConfigurations.cs
@@ -29,34 +29,7 @@ namespace Learnava.DataAccess.Data.Configurations
                 .HasForeignKey(c => c.InstructorId)
                 .IsRequired();
 
-            builder.HasData(new List<Course>()
-{
-    new Course()
-    {
-        Id = -1,
-        CreatedAt = new DateTime(2024, 6, 1),
-        Title = "Data Structures",
-        Description = "Learn Data Structures and use them to solve real world problems",
-        InstructorId = "e4d92a7b-15a4-4f68-a0f4-de7c0ea63064"
-    },
-    new Course()
-    {
-        Id = -2,
-        CreatedAt = new DateTime(2024, 6, 1),
-        Title = "Algorithms",
-        Description = "Learn Algorithms and apply them in practice",
-        InstructorId = "c0241e9f-63de-4005-9508-be5d2c1fb8e7"
-    },
-    new Course()
-    {
-        Id = -3,
-        CreatedAt = new DateTime(2024, 6, 1),
-        Title = "OOP",
-        Description = "Master Object-Oriented Programming fundamentals",
-        InstructorId = "03b35f2f-b6c6-48f8-8616-1ff545e6a864"
-    }
-});
-
+          
         }
     }
 }
