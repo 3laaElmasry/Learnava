50c3a2f90385af22d78749ba96e141d2b9df8603
Elmasry <3laaelmasry2005a@gmail.com>
2025-07-10 05:28:28 +0300

    IVideoService Contracts

--- Learnava.BusinessLogic/IServiceContracts/IVideoService.cs
+++ Learnava.BusinessLogic/IServiceContracts/IVideoService.cs
@@ -1,8 +1,19 @@
 ﻿
 
+using Learnava.DataAccess.Data.Entities;
+using System.Linq.Expressions;
+
 namespace Learnava.BusinessLogic.IServiceContracts
 {
-    public class IVideoService
+    public interface IVideoService
     {
+        Task<IEnumerable<Video>> GetVideosAsync(Expression<Func<Video,bool>>? filter = null, string? included = null);
+        Task<Video> GetVideoByIdAsync(int videoId);
+
+        Task<Video> UpdateVideoAsync(int videoId, Video newVideo);
+
+        Task<bool> DeleteVideoAsync(int videoId);
+
+        Task<Video> GetVideoAsync(Expression<Func<Video, bool>>? filter = null, string? included = null);
     }
 }
--- Learnava.BusinessLogic/Services/VideoService.cs
+++ Learnava.BusinessLogic/Services/VideoService.cs
@@ -1,9 +1,35 @@
 ﻿
 using Learnava.BusinessLogic.IServiceContracts;
+using Learnava.DataAccess.Data.Entities;
+using System.Linq.Expressions;
 
 namespace Learnava.BusinessLogic.Services
 {
     public class VideoService : IVideoService
     {
+        public Task<bool> DeleteVideoAsync(int videoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Video> GetVideoAsync(Expression<Func<Video, bool>>? filter = null, string? included = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Video> GetVideoByIdAsync(int videoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Video>> GetVideosAsync(Expression<Func<Video, bool>>? filter = null, string? included = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Video> UpdateVideoAsync(int videoId, Video newVideo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
